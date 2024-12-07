4e300d5599655f704fe6a290492c619ee3ff252d
jayb2302 <jonbja01@easv365.dk>
2024-12-07 22:26:15 +0100

    feat: implement light/dark theme toggle and pre-load logic to prevent flashing (style may be adjusted)

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 ?>
 <header class="header">
     <a class="header-brand" href="<?php echo url('/'); ?>">
-        <img src="<?= asset('assets/brand/LogoIdea.svg'); ?>" alt="Drinx Logo" width="" height="50" class="d-inline-block align-top">
+        <img src="<?= asset('assets/brand/LogoIdea.svg'); ?>" alt="Drinx Logo" width="" height="50" class="logo align-top">
     </a>
     <!-- Search Bar -->
     <div class="search-container">
@@ -15,4 +15,8 @@ if (session_status() === PHP_SESSION_NONE) {
             <div id="cocktailSuggestions"></div>
         </div>
     </div>
+    <button id="theme-toggle" class="theme-toggle-button">
+        <i class="fa-solid fa-sun" id="icon-sun"></i>
+        <i class="fa-solid fa-moon" id="icon-moon" style="display:none;"></i>
+    </button>
 </header>
