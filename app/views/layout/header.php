ed2ba83367214e2291ea9aecfa143d1aad20d0db
KianMiridoozini <114469799+KianMiridoozini@users.noreply.github.com>
2024-12-16 23:19:37 +0100

    Session handling adjusted
    User logged out after 2 weeks or when they close their browser.
    
    Js implemented to check for session status every hour. if the session is expired, the js will refresh the page to complete the logout process.
    
    - minor adjustments

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -1,7 +1,16 @@
 <?php include __DIR__ . '/head.php';
+
+// Ensure the session is started
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
+// Check for the session-expired cookie
+if (isset($_COOKIE['session_expired'])) {
+    echo '<div id="message" class="alert alert-danger"><i class="fa-solid fa-bell error"></i><h4>' . htmlspecialchars($_COOKIE['session_expired']) . '</h4></div>';
+    setcookie('session_expired', '', time() - 3600, '/'); // Clear the cookie
+}
+
 ?>
 <header class="header">
     <a class="header-brand" href="<?php echo url('/'); ?>">
